6a934ef10a2beb665aecca3f283a118e300a87cf
Rogerio A Serapiao <rserapa@gmail.com>
2021-09-28 20:48:06 -0300

    create and implement test validation property_location not_blanck and unique to app - not DB validations

--- app/models/property_location.rb
+++ app/models/property_location.rb
@@ -1,2 +1,5 @@
 class PropertyLocation < ApplicationRecord
+
+    validates :location, uniqueness: true , 
+        presence: { message: 'Erro, Região precisa possuir um nome e não pode ser em branco' }
 end
--- spec/system/visitor_register_property_location_spec.rb
+++ spec/system/visitor_register_property_location_spec.rb
@@ -12,4 +12,19 @@ describe 'Visitor register property location' do
         # Assert
         expect(page).to have_content("Litoral de Florianópolis")        
     end
+
+    it 'Created region failed' do
+        # Arrange
+
+        # Act
+        visit root_path
+        click_on 'Cadastrar Região'
+        fill_in "Região", with: "" 
+        click_on 'Cadastrar'
+
+        # Assert
+        expect(page).to have_content("Erro, Região precisa possuir um nome e não pode ser em branco")        
+    end
+
+
 end
